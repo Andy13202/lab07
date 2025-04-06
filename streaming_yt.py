580ab4b768ac7cb240a9a3b464ef1e37471d1880
Andy13202 <andy2001301@gmail.com>
2025-04-06 16:52:27 +0800

    Update streaming_yt.py

--- streaming_yt.py
+++ streaming_yt.py
@@ -3,8 +3,12 @@ from yt_dlp import YoutubeDL
 
 def play_video(url):
     ydl_opts = {
-        'format': 'best',
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'noplaylist': True,
+        'geo_bypass': True,  # 嘗試繞過地區限制
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
     }
     with YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
