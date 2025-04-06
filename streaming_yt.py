8ce6c1fc362bbe1506cab0d91fa8887b6e89e4c8
Andy13202 <andy2001301@gmail.com>
2025-04-06 16:55:01 +0800

    Update streaming_yt.py

--- streaming_yt.py
+++ streaming_yt.py
@@ -3,21 +3,26 @@ from yt_dlp import YoutubeDL
 
 def play_video(url):
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'format': 'bestvideo+bestaudio/best',  # 获取最佳视频和最佳音频，或者最佳整体质量的流
         'noplaylist': True,
-        'geo_bypass': True,  # 嘗試繞過地區限制
+        'geo_bypass': True,
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
         }
     }
     with YoutubeDL(ydl_opts) as ydl:
         info_dict = ydl.extract_info(url, download=False)
-        video_url = info_dict['url']
+        if 'url' in info_dict:
+            video_url = info_dict['url']  # 直接获取URL
+        else:
+            # 如果顶层没有URL，从formats中选择一个URL
+            video_url = info_dict['formats'][0]['url']  # 选择第一个格式的URL
+
         os.system(f"mpv '{video_url}'")
 
 def main():
-    # YouTube視頻URL
-    url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # 這裡放你要播放的YouTube視頻連結
+    # YouTube视频URL
+    url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'  # 这里放你要播放的YouTube视频链接
     play_video(url)
 
 if __name__ == "__main__":
